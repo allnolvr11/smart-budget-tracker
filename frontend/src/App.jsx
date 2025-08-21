3f032c01759d3019e690405cc88de157ef37e27a
allnolvr11 <118269435+allnolvr11@users.noreply.github.com>
2025-08-21 19:24:58 +0800

    setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Dashboard = React.lazy(()=> import('./pages/Dashboard'));
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const router = createBrowserRouter ([
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={null}>
+          <Home />
+        </Suspense>
+      )
+    },
+    {
+      path: "/dashboard",
+      element: (
+        <Suspense fallback={null}>
+          <Dashboard />
+        </Suspense>
+      )
+    }
+  ]);
+
+  return <RouterProvider router={router}/> ;
 }
 
 export default App
